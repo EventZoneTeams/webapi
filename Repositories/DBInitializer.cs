aae7eb046b1c9f54316dd2468bf56f5cd5f1eebd
Le Quoc Uy <lequocuyit@gmail.com>
2024-05-18 15:30:38 +0700

    add table into user table

--- Repositories/DBInitializer.cs
+++ Repositories/DBInitializer.cs
@@ -55,6 +55,81 @@ namespace Repositories
                 await userManager.CreateAsync(student, "123456");
                 await userManager.AddToRoleAsync(student, "Student");
 
+                var uydev = new User
+                {
+                    UserName = "uydev",
+                    Email = "lequocuy@gmail.com",
+                    FullName = "Le Quoc Uy",
+                    University = "FPTU HCM",
+                    PointBalance = 1000,
+                    Dob = new DateTime(2003, 7, 11),
+                    Gender = true,
+                    Image = "https://scontent.fsgn15-1.fna.fbcdn.net/v/t39.30808-1/430878538_2206677789683723_4464660377243750146_n.jpg?stp=dst-jpg_p200x200&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeE_Vr1x6BHZ_S__ovdDg7zS5W9udhABzaHlb252EAHNoS38q_urtNeTErRYpa0zqYNo-vOAf49-zjjLBslYOw-p&_nc_ohc=8En2AdNVtaUQ7kNvgEn1g25&_nc_ht=scontent.fsgn15-1.fna&oh=00_AYA_Dyr3Kzs4J5lFKCiaYlu6-KlRK4icdur4m-IrU68PPA&oe=664E1D9B"
+                };
+                //Add roles
+                await userManager.CreateAsync(uydev, "123456");
+                await userManager.AddToRoleAsync(uydev, "Student");
+
+                var namthhse172294 = new User
+                {
+                    UserName = "namthhse172294",
+                    Email = "namthhse172294@fpt.edu.vn",
+                    FullName = "Trương Hà Hào Nam",
+                    University = "FPTU HCM",
+                    PointBalance = 1000,
+                    Dob = new DateTime(2003, 1, 1), // Replace with the actual date of birth
+                    Gender = true, // Assuming true means male
+                    Image = "https://avatar.iran.liara.run/public/boy?username=namthhse172294" // Replace with the actual image URL
+                };
+
+                await userManager.CreateAsync(namthhse172294, "123456"); // Replace "password" with the actual password
+                await userManager.AddToRoleAsync(namthhse172294, "Student");
+
+                var vunse172437 = new User
+                {
+                    UserName = "vunse172437",
+                    Email = "vunse172437@fpt.edu.vn",
+                    FullName = "Nguyễn Vũ",
+                    University = "FPTU HCM",
+                    PointBalance = 1000,
+                    Dob = new DateTime(2003, 2, 15), // Replace with the actual date of birth
+                    Gender = true, // Assuming true means male
+                    Image = "https://avatar.iran.liara.run/public/boy?username=vunse172437" // Replace with the actual image URL
+                };
+
+                await userManager.CreateAsync(vunse172437, "123456"); // Replace "password" with the actual password
+                await userManager.AddToRoleAsync(vunse172437, "Student");
+
+                var huanngse171018 = new User
+                {
+                    UserName = "huanngse171018",
+                    Email = "huanngse171018@fpt.edu.vn",
+                    FullName = "Ngô Gia Huấn",
+                    University = "FPTU HCM",
+                    PointBalance = 1000,
+                    Dob = new DateTime(2003, 3, 20), // Replace with the actual date of birth
+                    Gender = true, // Assuming true means male
+                    Image = "https://avatar.iran.liara.run/public/boy?username=huanngse171018" // Replace with the actual image URL
+                };
+
+                await userManager.CreateAsync(huanngse171018, "123456"); // Replace "password" with the actual password
+                await userManager.AddToRoleAsync(huanngse171018, "Student");
+
+                var tienhmse172436 = new User
+                {
+                    UserName = "tienhmse172436",
+                    Email = "tienhmse172436@fpt.edu.vn",
+                    FullName = "Hoàng Minh Tiến",
+                    University = "FPTU HCM",
+                    PointBalance = 1000,
+                    Dob = new DateTime(2003, 4, 5), // Replace with the actual date of birth
+                    Gender = true, // Assuming true means male
+                    Image = "https://avatar.iran.liara.run/public/boy?username=tienhmse172436" // Replace with the actual image URL
+                };
+
+                await userManager.CreateAsync(tienhmse172436, "123456"); // Replace "password" with the actual password
+                await userManager.AddToRoleAsync(tienhmse172436, "Student");
+
                 await context.SaveChangesAsync();
             }
         }
