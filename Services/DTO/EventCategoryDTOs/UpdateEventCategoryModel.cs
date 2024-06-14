5c683d492ae6c71a1639c4b7be82fddeb9864857
Le Quoc Uy <91858657+UyLeQuoc@users.noreply.github.com>
2024-06-14 14:47:17 +0700

    Feature/get order (#42)
    * done get 1 order
    
    * done purchase order
    
    * convert kebab event and event category

--- Services/DTO/EventCategoryDTOs/UpdateEventCategoryModel.cs
+++ Services/DTO/EventCategoryDTOs/UpdateEventCategoryModel.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Services.DTO.EventCategoryDTOs
 {
     public class UpdateEventCategoryModel
     {
+        [BindProperty(Name = "title")]
         public string Title { get; set; }
+        [BindProperty(Name = "image")]
         public IFormFile? Image { get; set; }
     }
 }
