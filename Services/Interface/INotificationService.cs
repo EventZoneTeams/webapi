5bd1e9797e08e0dfb6194889d2b2f3f74ed5cb34
Le Quoc Uy <lequocuyit@gmail.com>
2024-06-17 17:57:56 +0700

    done push message in service

--- Services/Interface/INotificationService.cs
+++ Services/Interface/INotificationService.cs
@@ -6,6 +6,7 @@ namespace Services.Interface
     {
         Task<List<NotificationDTO>> GetNotifications(int userId);
         Task<int> GetUnreadNotificationQuantity(int userId);
+        Task PushNotification(int userId, string title, string body);
         Task<List<NotificationDTO>> ReadAllNotification(int userId);
     }
 }
--- Services/Services/NotificationService.cs
+++ Services/Services/NotificationService.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using Domain.Entities;
+using Microsoft.AspNetCore.SignalR;
 using Repositories.Interfaces;
 using Services.DTO.NotificationDTOs;
+using Services.Hubs;
 using Services.Interface;
 
 namespace Services.Services
@@ -9,12 +12,34 @@ namespace Services.Services
     {
         private readonly IUnitOfWork _unitOfWork;
         private readonly IMapper _mapper;
+        private readonly IHubContext<NotificationHub> _notificationHubContext;
 
-        public NotificationService(IUnitOfWork unitOfWork, IMapper mapper)
+        public NotificationService(IUnitOfWork unitOfWork, IMapper mapper, IHubContext<NotificationHub> notificationHubContext)
         {
             _unitOfWork = unitOfWork;
             _mapper = mapper;
+            _notificationHubContext = notificationHubContext;
         }
+
+        public async Task PushNotification(int userId, string title, string body)
+        {
+            var newNotification = new Notification
+            {
+                Title = title,
+                Body = body,
+                UserId = userId,
+                IsRead = false,
+                Url = null,
+                Sender = "System"
+            };
+            //save notification to DB
+            await _unitOfWork.NotificationRepository.AddAsync(newNotification);
+            await _unitOfWork.SaveChangeAsync();
+
+            //push noti to signalR
+            await _notificationHubContext.Clients.All.SendAsync("sendToUser", title, body);
+        }
+
 
         public async Task<List<NotificationDTO>> GetNotifications(int userId)
         {
