c876ed5896d3f147b814177662449d63fe3bd31c
Le Quoc Uy <lequocuyit@gmail.com>
2024-07-05 14:23:51 +0700

    change response DTO

--- Services/DTO/EventDTOs/EventResponseDTO.cs
+++ Services/DTO/EventDTOs/EventResponseDTO.cs
@@ -1,6 +1,4 @@
-﻿using Domain.Entities;
-using Domain.Enums;
-using Repositories.Models;
+﻿using Repositories.Models;
 using Services.DTO.EventCampaignDTOs;
 using Services.DTO.EventCategoryDTOs;
 
@@ -20,7 +18,7 @@ namespace Services.DTO.EventDTOs
         public UserDetailsModel User { get; set; }
         public EventCategoryResponseDTO? EventCategory { get; set; }
         public string University { get; set; }
-        public EventStatusEnums? Status { get; set; }
+        public string? Status { get; set; }
         public Int64 TotalCost { get; set; }
         public DateTime CreatedAt { get; set; }
         public bool IsDeleted { get; set; }
