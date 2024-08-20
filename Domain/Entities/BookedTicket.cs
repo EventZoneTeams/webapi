f41d13c74a772f9bfa78887fbe2380a416981ade
Le Quoc Uy <lequocuyit@gmail.com>
2024-08-20 12:00:25 +0700

    add ticket table

---
+++ Domain/Entities/BookedTicket.cs
@@ -0,0 +1,17 @@
+﻿namespace EventZone.Domain.Entities
+{
+    public class BookedTicket : BaseEntity
+    {
+        public Guid EventTicketId { get; set; }
+        public Guid EventId { get; set; }
+        public Guid UserId { get; set; }
+        public Int64 PaidPrice { get; set; }
+        public string AttendeeNote { get; set; }
+        public bool IsCheckedIn { get; set; }
+        public Guid EventOrderId { get; set; }
+        public virtual Event Event { get; set; }
+        public virtual User User { get; set; }
+        public virtual EventTicket EventTicket { get; set; }
+        public virtual EventOrder EventOrder { get; set; }
+    }
+}
---
+++ Domain/Entities/EventTicket.cs
@@ -0,0 +1,12 @@
+﻿namespace EventZone.Domain.Entities
+{
+    public class EventTicket : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Guid EventId { get; set; }
+        public int InStock { get; set; }
+        public Int64 Price { get; set; }
+        public virtual Event Event { get; set; }
+    }
+}
