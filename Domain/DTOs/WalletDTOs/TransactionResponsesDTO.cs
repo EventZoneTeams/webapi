944e8c0359e66eb98c3f254efa1ca2a41d8f9997
Hoang Tien <minhtien11052003@gmail.com>
2024-09-29 17:03:28 +0700

    fix: fix wallet dtos

--- Domain/DTOs/WalletDTOs/TransactionResponsesDTO.cs
+++ Domain/DTOs/WalletDTOs/TransactionResponsesDTO.cs
@@ -2,8 +2,8 @@
 {
     public class TransactionResponsesDTO
     {
-        public int Id { get; set; }
-        public int WalletId { get; set; }
+        public Guid Id { get; set; }
+        public Guid WalletId { get; set; }
         public string TransactionType { get; set; }
         public long Amount { get; set; }
         public string? Description { get; set; }
