9da5a72fc44e3b5fc012285f93dc857e843e1f31
Hoang Tien <minhtien11052003@gmail.com>
2024-10-01 12:22:54 +0700

    add: handle product quantity

--- Domain/DTOs/EventOrderDTOs/CreateEventOrderDetailsReponseDTO.cs
+++ Domain/DTOs/EventOrderDTOs/CreateEventOrderDetailsReponseDTO.cs
@@ -1,8 +1,14 @@
-﻿namespace EventZone.Domain.DTOs.EventOrderDTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventZone.Domain.DTOs.EventOrderDTOs
 {
     public class CreateEventOrderDetailsReponseDTO
     {
+        [Required(ErrorMessage = "EventProductId is required")]
         public Guid EventProductId { get; set; }
-        public int Quantity { get; set; } = 1;
+
+        [Range(1, 10, ErrorMessage = "Quantity must be greater than 0 and lower than 10")]
+        [Required(ErrorMessage = "Quantity is required")]
+        public int Quantity { get; set; }
     }
 }
