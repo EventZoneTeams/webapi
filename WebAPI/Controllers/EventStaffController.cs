1b7a39431f16d7d9185fd1f4796e88758762ee0d
Le Quoc Uy <lequocuyit@gmail.com>
2024-10-13 19:57:27 +0700

    fix v1

--- WebAPI/Controllers/EventStaffController.cs
+++ WebAPI/Controllers/EventStaffController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace EventZone.WebAPI.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     public class EventStaffController : Controller
     {
         private readonly IEventStaffService _eventStaffService;
